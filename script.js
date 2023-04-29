1ed6f6af2a026739ee2e09804f91872d604ab56b
HEGGIX <glebermakovich@gmail.com>
2023-04-30 01:15:48 +0300

    fix: add keycode config

--- script.js
+++ script.js
@@ -1,24 +1,466 @@
-const keyContainer = document.createElement("div");
-const keyboard = document.createElement("div");
-const body = document.getElementById("body");
-keyboard.classList.add("keyboard");
-keyContainer.classList.add("keyboard__cont");
-document.body.appendChild(keyContainer);
-document.body.appendChild(keyboard);
-
-
-const keyArrEng = [
-    "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-    "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]",
-    "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-    "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "shift",
-    "ctrl", "alt", "space" ,"alt" , "ctrl"
-];
-
-const keyArrRus = [
-    "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-    "tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ",
-    "caps", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "enter",
-    "shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "?", "shift",
-    "ctrl", "alt", "space" ,"alt" , "ctrl"
-];
+const config = [
+    [
+        {
+            keyCode: 192,
+            lang: {
+                ru: '~',
+                en: '`'
+            }
+        },
+        {
+            keyCode: 49,
+            lang: {
+                ru: '1',
+                en: '1'
+            },
+        },
+        {
+            keyCode: 50,
+            lang: {
+                ru: '2',
+                en: '2'
+            },
+        },
+        {
+            keyCode: 51,
+            lang: {
+                ru: '3',
+                en: '3'
+            },
+        },
+        {
+            keyCode: 52,
+            lang: {
+                ru: '4',
+                en: '4'
+            },
+        },
+        {
+            keyCode: 53,
+            lang: {
+                ru: '5',
+                en: '5'
+            },
+        },
+        {
+            keyCode: 54,
+            lang: {
+                ru: '6',
+                en: '6'
+            },
+        },
+        {
+            keyCode: 55,
+            lang: {
+                ru: '7',
+                en: '7'
+            },
+        },
+        {
+            keyCode: 56,
+            lang: {
+                ru: '8',
+                en: '8'
+            },
+        },
+        {
+            keyCode: 57,
+            lang: {
+                ru: '9',
+                en: '9'
+            },
+        },
+        {
+            keyCode: 58,
+            lang: {
+                ru: '0',
+                en: '0'
+            },
+        },
+        {
+            keyCode: 189,
+            lang: {
+                ru: '-',
+                en: '-'
+            },
+        },
+        {
+            keyCode: 187,
+            lang: {
+                en: '=',
+                ru: '='
+            },
+        },
+        {
+            keyCode: 8,
+            lang: {
+                en: 'backspace',
+                ru: 'backspace'
+            },
+            style: 'backspace'
+        }
+    ],
+    [
+        {
+            keyCode: 9,
+            lang: {
+                en: 'tab',
+                ru: 'tab'
+            }
+        },
+        {
+            keyCode: 81,
+            lang: {
+                en: 'q',
+                ru: 'й',
+            }
+        },
+        {
+            keyCode: 87,
+            lang: {
+                en: 'w',
+                ru: 'ц'
+            }
+        },
+        {
+            keyCode: 69,
+            lang: {
+                en: 'e',
+                ru: 'у'
+            }
+        },
+        {
+            keyCode: 82,
+            lang: {
+                en: 'r',
+                ru: 'к'
+            }
+        },
+        {
+            keyCode: 84,
+            lang: {
+                en: 't',
+                ru: 'е'
+            }
+        },
+        {
+            keyCode: 89,
+            lang: {
+                en: 'y',
+                ru: 'н'
+            }
+        },
+        {
+            keyCode: 85,
+            lang: {
+                en: 'u',
+                ru: 'г'
+            }
+        },
+        {
+            keyCode: 73,
+            lang: {
+                en: 'i',
+                ru: 'ш'
+            }
+        },
+        {
+            keyCode: 79,
+            lang: {
+                en: 'o',
+                ru: 'щ'
+            }
+        },
+        {
+            keyCode: 80,
+            lang: {
+                en: 'p',
+                ru: 'з'
+            }
+        },
+        {
+            keyCode: 219,
+            lang: {
+                en: '[',
+                ru: 'х'
+            }
+        },
+        {
+            keyCode: 221,
+            lang: {
+                en: ']',
+                ru: 'ъ'
+            }
+        },
+        {
+            keyCode: 220,
+            lang: {
+                en: '\\',
+                ru: '\\'
+            }
+        },
+        {
+            keyCode: 46,
+            lang: {
+                en: 'del',
+                ru: 'del'
+            }
+        },
+    ],
+    [
+        {
+            keyCode: 20,
+            lang: {
+                en: 'CapsLock',
+                ru: 'CapsLock'
+            },
+            style: 'capslock'
+        },
+        {
+            keyCode: 65,
+            lang: {
+                en: 'a',
+                ru: 'ф'
+            }
+        },
+        {
+            keyCode: 83,
+            lang: {
+                en: 's',
+                ru: 'ы'
+            }
+        },
+        {
+            keyCode: 68,
+            lang: {
+                en: 'd',
+                ru: 'в'
+            }
+        },
+        {
+            keyCode: 70,
+            lang: {
+                en: 'f',
+                ru: 'а'
+            }
+        },
+        {
+            keyCode: 71,
+            lang: {
+                en: 'g',
+                ru: 'п'
+            }
+        },
+        {
+            keyCode: 72,
+            lang: {
+                en: 'h',
+                ru: 'р'
+            }
+        },
+        {
+            keyCode: 74,
+            lang: {
+                en: 'j',
+                ru: 'о'
+            }
+        },
+        {
+            keyCode: 75,
+            lang: {
+                en: 'k',
+                ru: 'л'
+            }
+        },
+        {
+            keyCode: 76,
+            lang: {
+                en: 'l',
+                ru: 'д'
+            }
+        },
+        {
+            keyCode: 186,
+            lang: {
+                en: ';',
+                ru: 'ж'
+            }
+        },
+        {
+            keyCode: 222,
+            lang: {
+                en: '\'',
+                ru: 'э'
+            },
+        },
+        {
+            keyCode: 13,
+            lang: {
+                en: 'Enter',
+                ru: 'Enter'
+            },
+            style: 'enter'
+        }
+    ],
+    [
+        {
+            keyCode: 16,
+            lang: {
+                en: 'Shift',
+                ru: 'Shift'
+            },
+            style: 'shift'
+        },
+        {
+            keyCode: 90,
+            lang: {
+                en: 'z',
+                ru: 'я'
+            },
+        },
+        {
+            keyCode: 88,
+            lang: {
+                en: 'x',
+                ru: 'ч'
+            },
+        },
+        {
+            keyCode: 67,
+            lang: {
+                en: 'c',
+                ru: 'с'
+            },
+        },
+        {
+            keyCode: 86,
+            lang: {
+                en: 'v',
+                ru: 'м'
+            },
+        },
+        {
+            keyCode: 66,
+            lang: {
+                en: 'b',
+                ru: 'и'
+            },
+        },
+        {
+            keyCode: 78,
+            lang: {
+                en: 'n',
+                ru: 'т'
+            },
+        },
+        {
+            keyCode: 77,
+            lang: {
+                en: 'm',
+                ru: 'ь'
+            },
+        },
+        {
+            keyCode: 188,
+            lang: {
+                en: ',',
+                ru: 'б'
+            },
+        },
+        {
+            keyCode: 190,
+            lang: {
+                en: '.',
+                ru: 'ю'
+            },
+        },
+        {
+            keyCode: 191,
+            lang: {
+                en: '/',
+                ru: '.'
+            },
+        },
+        {
+            keyCode: 38,
+            lang: {
+                en: 'Up',
+                ru: 'Up'
+            },
+        },
+        {
+            keyCode: 16,
+            lang: {
+                en: 'Shift',
+                ru: 'Shift'
+            },
+            style: 'shift'
+        },
+    ],
+    [
+        {
+            keyCode: 17,
+            lang: {
+                en: 'Ctrl',
+                ru: 'Ctrl'
+            },
+        },
+        {
+            keyCode: 91,
+            lang: {
+                en: 'Win',
+                ru: 'Win'
+            },
+        },
+        {
+            keyCode: 18,
+            lang: {
+                en: 'Alt',
+                ru: 'Alt'
+            },
+        },
+        {
+            keyCode: 32,
+            lang: {
+                en: 'Space',
+                ru: 'Space'
+            },
+            style: 'space'
+        },
+        {
+            keyCode: 18,
+            lang: {
+                en: 'Alt',
+                ru: 'Alt'
+            },
+        },
+        {
+            keyCode: 37,
+            lang: {
+                en: 'Left',
+                ru: 'Left'
+            },
+        },
+        {
+            keyCode: 40,
+            lang: {
+                en: 'Down',
+                ru: 'Down'
+            },
+        },
+        {
+            keyCode: 39,
+            lang: {
+                en: 'Right',
+                ru: 'Right'
+            },
+        },
+        {
+            keyCode: 17,
+            lang: {
+                en: 'Ctrl',
+                ru: 'Ctrl'
+            },
+        }
+    ]
+]
